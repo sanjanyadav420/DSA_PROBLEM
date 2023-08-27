a946840b4302c1cbbbcb486aad84a336213a6187
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-08-27 14:21:49 +0530

    add

--- CMATH.cpp
+++ CMATH.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main(){
    cout<<sqrt(24)<<"\n";
    cout<<pow(4,5)<<endl;
-
-
 //  here no double value becoz here datatype is int so that why
 int a= sqrt(24);
 cout<<a<<endl;
