b508ed57e5cba9b3533332a5fb33e5d6592029c0
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-08-27 13:39:11 +0530

    add

--- Sum.cpp
+++ Sum.cpp
@@ -3,11 +3,18 @@ using namespace std;
 int add(int num1,int num2){
     int sum=num1+num2;
     return sum;
-    
+
+}
+int add(int n ,int n1, int n2){
+    int sum= n+n1+n2;
+    return sum;
+
 }
 
 int main(){
     int a =5;
     int b=8;
    cout<<add(a,b)<<endl;
+   cout<<add(a,b,5)<<endl;
+   
 }
