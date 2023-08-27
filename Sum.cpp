4bb4f61ebbf0fe308aa284c4e2955e298424f456
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-08-27 13:45:53 +0530

    add

--- Sum.cpp
+++ Sum.cpp
@@ -10,11 +10,22 @@ int add(int n ,int n1, int n2){
     return sum;
 
 }
+int add(float n,float n1 ){
+    int sum= n+n1;
+    return sum;
+
+}
 
 int main(){
     int a =5;
     int b=8;
+    float c=3.4;
+    float d=9.5;
+
+    
+
    cout<<add(a,b)<<endl;
    cout<<add(a,b,5)<<endl;
-   
+   cout<<add(c,d)<<endl;
+
 }
