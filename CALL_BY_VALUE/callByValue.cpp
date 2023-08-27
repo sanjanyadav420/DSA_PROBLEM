fc106c60164685f94ed16be546c2de505022ee62
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-08-27 14:50:41 +0530

    init

--- CALL_BY_VALUE/callByValue.cpp
+++ CALL_BY_VALUE/callByValue.cpp
@@ -3,11 +3,13 @@ using namespace std;
 void change(int data);  
 int main()  
 {  
+    
 int data = 3;  
 change(data);  
 cout << "Value of the data is: " << data<< endl;  
 return 0;  
 }  
+
 void change(int data)  
 {  
 data = 5;  
