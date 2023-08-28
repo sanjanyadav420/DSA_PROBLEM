f2dfab918166c7a6998a9b7d10021070ff099116
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-08-28 21:56:59 +0530

    init

---
+++ PrimeNumber/primeNumber.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void prime(int n){
+    int c=0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+              c++;
+         }
+    }
+    if(c==2){
+        cout<<"Yes Number is prime"<<endl;
+    }
+    else{
+        cout<<" number is not prime"<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"check number is prime or not"<<endl;
+    cin>>n;
+    prime(n);
+}
