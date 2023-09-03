afce7d40b15e7d5fbf8f8849385dcb7866a816f7
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-09-03 21:44:54 +0530

    add

--- PrimeNumber/primeNumber.cpp
+++ PrimeNumber/primeNumber.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 void prime(int n){
     int c=0;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=n;++i){
         if(n%i==0){
               c++;
          }
     }
+
     if(c==2){
         cout<<"Yes Number is prime"<<endl;
     }
@@ -17,7 +18,7 @@ void prime(int n){
 int main(){
 
     int n;
-    
+
     cout<<"check number is prime or not"<<endl;
     cin>>n;
     prime(n);
