253a8cf8410dcb062030df4457a1282ed6b654cc
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-09-17 00:08:28 +0530

    add

--- Sorting/insertionSort/insertion.cpp
+++ Sorting/insertionSort/insertion.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 void insertionSort(int arr[], int n) {
-    
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        // Move elements of arr[0..i-1] that are greater than key
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
+    for(int i=1;i<n;i++){
+        int curnt=arr[i];
+        int j=i-1;
+        while (j>=0 && arr[j]>curnt)
+        {
+            arr[j+1]=arr[j];
             j--;
         }
-        arr[j + 1] = key;
+        arr[j+1]=curnt;
+
+        
     }
+
+   
 }
 
 int main() {
