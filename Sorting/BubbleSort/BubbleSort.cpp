dcf46300c85b69ee39fc1558f10ea1ed3e975564
Sanjan Yadav <sanjanyadav060@gmail.com>
2023-09-16 22:58:33 +0530

    add

--- Sorting/BubbleSort/BubbleSort.cpp
+++ Sorting/BubbleSort/BubbleSort.cpp
@@ -2,14 +2,22 @@
 
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
+int bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; ++i) {
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+               int swap = arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=swap;
             }
         }
     }
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
 }
 
 int main() {
@@ -31,11 +39,6 @@ int main() {
 
     bubbleSort(arr, n);
 
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+   
 
-    return 0;
 }
